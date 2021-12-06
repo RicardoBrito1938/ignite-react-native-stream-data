678283fd8e78cc2e9b10ee662f061a241fa5b991
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2021-12-06 19:06:01 +0000

    signOut

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -94,14 +94,21 @@ function AuthProvider({ children }: AuthProviderData) {
 
   async function signOut() {
     try {
-      // set isLoggingOut to true
-      // call revokeAsync with access_token, client_id and twitchEndpoint revocation
+      setIsLoggingIn(true);
+      await revokeAsync(
+        { token: userToken, clientId: CLIENT_ID },
+        {
+          revocationEndpoint: twitchEndpoints.revocation
+        }
+      );
     } catch (error) {
     } finally {
-      // set user state to an empty User object
-      // set userToken state to an empty string
-      // remove "access_token" from request's authorization header
-      // set isLoggingOut to false
+      setUser({} as User);
+      setUserToken("");
+
+      delete api.defaults.headers.authorization;
+
+      setIsLoggingOut(false);
     }
   }
 
